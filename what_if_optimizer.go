007e29d5ccd947f481b53551482f71feba30421d
qw4990 <zhangyuanjia@pingcap.com>
2023-05-24 21:23:40 +0800

    what-if

--- what_if_optimizer.go
+++ what_if_optimizer.go
@@ -1,37 +1,83 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type WhatIfOptimizer interface {
 	Execute(sql string) error
+	Close() error // release the underlying database connection
 
-	CreateIndex(tableName string, indexName string, columnNames []string) error
-	DropIndex(tableName string, indexName string) error
+	CreateHypoIndex(schemaName, tableName, indexName string, columnNames []string) error
+	DropHypoIndex(schemaName, tableName, indexName string) error
 
-	GetPlan(query string) (planText string, err error)
 	GetPlanCost(query string) (planCost float64, err error)
 }
 
 type TiDBWhatIfOptimizer struct {
+	db *sql.DB
 }
 
 func NewTiDBWhatIfOptimizer(DSN string) (WhatIfOptimizer, error) {
-	return &TiDBWhatIfOptimizer{}, nil
+	db, err := sql.Open("mysql", DSN)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
+
+	return &TiDBWhatIfOptimizer{db}, nil
 }
 
 func (o *TiDBWhatIfOptimizer) Execute(sql string) error {
-	return nil
+	_, err := o.db.Exec(sql)
+	return err
 }
 
-func (o *TiDBWhatIfOptimizer) CreateIndex(tableName string, indexName string, columnNames []string) error {
-	return nil
-}
-func (o *TiDBWhatIfOptimizer) DropIndex(tableName string, indexName string) error {
-	return nil
+func (o *TiDBWhatIfOptimizer) Close() error {
+	return o.db.Close()
 }
 
-func (o *TiDBWhatIfOptimizer) GetPlan(query string) (planText string, err error) {
-	return "", nil
+func (o *TiDBWhatIfOptimizer) CreateHypoIndex(schemaName, tableName, indexName string, columnNames []string) error {
+	return o.Execute(fmt.Sprintf(`create index %v type hypo on %v.%v (%v)`, indexName, schemaName, tableName, strings.Join(columnNames, ", ")))
+}
+func (o *TiDBWhatIfOptimizer) DropHypoIndex(schemaName, tableName, indexName string) error {
+	return o.Execute(fmt.Sprintf("drop index %v on %v.%v", indexName, schemaName, tableName))
+}
+
+func (o *TiDBWhatIfOptimizer) getPlan(query string) (plan [][]string, err error) {
+	//	mysql> explain format='verbose' select * from t;
+	//	+-----------------------+----------+------------+-----------+---------------+--------------------------------+
+	//	| id                    | estRows  | estCost    | task      | access object | operator info                  |
+	//	+-----------------------+----------+------------+-----------+---------------+--------------------------------+
+	//	| TableReader_5         | 10000.00 | 177906.67  | root      |               | data:TableFullScan_4           |
+	//	| └─TableFullScan_4     | 10000.00 | 2035000.00 | cop[tikv] | table:t       | keep order:false, stats:pseudo |
+	//	+-----------------------+----------+------------+-----------+---------------+--------------------------------+
+	result, err := o.db.Query("explain format = 'verbose' " + query)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+	for result.Next() {
+		var id, estRows, estCost, task, obj, opInfo string
+		if err = result.Scan(&id, &estRows, &estCost, &task, &obj, &opInfo); err != nil {
+			return
+		}
+		plan = append(plan, []string{id, estRows, estCost, task, obj, opInfo})
+	}
+	return
 }
 
 func (o *TiDBWhatIfOptimizer) GetPlanCost(query string) (planCost float64, err error) {
-	return 0, nil
+	plan, err := o.getPlan(query)
+	if err != nil {
+		return 0, err
+	}
+	return strconv.ParseFloat(plan[0][2], 64)
 }
---
+++ what_if_optimizer_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWhatIfOptimizer(t *testing.T) {
+	dsn := "mysql://root:@127.0.0.1:4000/test"
+	o, err := NewTiDBWhatIfOptimizer(dsn)
+	must(err)
+	defer o.Close()
+	must(o.Execute(`create table t (a int, b int)`))
+	cost1, err := o.GetPlanCost(`select * from t where a=1`)
+	must(err)
+	must(o.CreateHypoIndex("test", "t", "idx_a", []string{"a"}))
+	cost2, err := o.GetPlanCost(`select * from t where a=1`)
+	must(err)
+	must(o.DropHypoIndex("test", "t", "idx_a"))
+	cost3, err := o.GetPlanCost(`select * from t where a=1`)
+	must(err)
+	fmt.Println(cost1, cost2, cost3) // cost2 > cost1 = cost3
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
