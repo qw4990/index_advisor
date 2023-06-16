8ca35de49bef38498c6d790a4b18ec6c147f340b
qw4990 <zhangyuanjia@pingcap.com>
2023-06-16 16:38:14 +0800

    refactor

--- workload/test/queries.sql
+++
@@ -1,8 +0,0 @@
-select * from test.test_t1 where a in (1, 2, 3);
-select * from test.test_t1 where a=1 and b=2 and c=3;
-select * from test.test_t1 where a=1 and d<10;
-select * from test.test_t2 where a>10;
-select * from test.test_t2 where a>1 and b=2 and c=3;
-select * from test.test_t2 where a>1 and d<10;
-select * from test.test_t1, test.test_t2 where test_t1.a=test_t2.a and test_t1.b<10 and test_t2.b<10;
-select * from test.test_t1, test.test_t2 where test_t1.a=test_t2.a and test_t1.d in (1, 2, 3) and test_t2.c=10;
--- workload/test/schema.sql
+++
@@ -1,2 +0,0 @@
-create table test.test_t1 (a int, b int, c int, d int);
-create table test.test_t2 (a int, b int, c int, d int);
