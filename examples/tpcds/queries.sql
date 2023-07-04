1e0bd889e72cde728f74c0ec912fdc6dcb623040
qw4990 <zhangyuanjia@pingcap.com>
2023-07-04 20:18:28 +0800

    update tpcds/queries.sql

--- examples/tpcds/queries.sql
+++ examples/tpcds/queries.sql
@@ -882,112 +882,6 @@ from(
 group by rollup (channel, i_brand_id,i_class_id,i_category_id)
 order by channel,i_brand_id,i_class_id,i_category_id
     limit 100;
-with  cross_items as
-          (select i_item_sk ss_item_sk
-           from item,
-                (select iss.i_brand_id brand_id
-                      ,iss.i_class_id class_id
-                      ,iss.i_category_id category_id
-                 from store_sales
-                    ,item iss
-                    ,date_dim d1
-                 where ss_item_sk = iss.i_item_sk
-                   and ss_sold_date_sk = d1.d_date_sk
-                   and d1.d_year between 1999 AND 1999 + 2
-                 intersect
-                 select ics.i_brand_id
-                      ,ics.i_class_id
-                      ,ics.i_category_id
-                 from catalog_sales
-                    ,item ics
-                    ,date_dim d2
-                 where cs_item_sk = ics.i_item_sk
-                   and cs_sold_date_sk = d2.d_date_sk
-                   and d2.d_year between 1999 AND 1999 + 2
-                 intersect
-                 select iws.i_brand_id
-                      ,iws.i_class_id
-                      ,iws.i_category_id
-                 from web_sales
-                    ,item iws
-                    ,date_dim d3
-                 where ws_item_sk = iws.i_item_sk
-                   and ws_sold_date_sk = d3.d_date_sk
-                   and d3.d_year between 1999 AND 1999 + 2) x
-           where i_brand_id = brand_id
-             and i_class_id = class_id
-             and i_category_id = category_id
-          ),
-      avg_sales as
-          (select avg(quantity*list_price) average_sales
-           from (select ss_quantity quantity
-                      ,ss_list_price list_price
-                 from store_sales
-                    ,date_dim
-                 where ss_sold_date_sk = d_date_sk
-                   and d_year between 1999 and 1999 + 2
-                 union all
-                 select cs_quantity quantity
-                      ,cs_list_price list_price
-                 from catalog_sales
-                    ,date_dim
-                 where cs_sold_date_sk = d_date_sk
-                   and d_year between 1999 and 1999 + 2
-                 union all
-                 select ws_quantity quantity
-                      ,ws_list_price list_price
-                 from web_sales
-                    ,date_dim
-                 where ws_sold_date_sk = d_date_sk
-                   and d_year between 1999 and 1999 + 2) x)
-select  this_year.channel ty_channel
-     ,this_year.i_brand_id ty_brand
-     ,this_year.i_class_id ty_class
-     ,this_year.i_category_id ty_category
-     ,this_year.sales ty_sales
-     ,this_year.number_sales ty_number_sales
-     ,last_year.channel ly_channel
-     ,last_year.i_brand_id ly_brand
-     ,last_year.i_class_id ly_class
-     ,last_year.i_category_id ly_category
-     ,last_year.sales ly_sales
-     ,last_year.number_sales ly_number_sales
-from
-    (select 'store' channel, i_brand_id,i_class_id,i_category_id
-          ,sum(ss_quantity*ss_list_price) sales, count(*) number_sales
-     from store_sales
-        ,item
-        ,date_dim
-     where ss_item_sk in (select ss_item_sk from cross_items)
-       and ss_item_sk = i_item_sk
-       and ss_sold_date_sk = d_date_sk
-       and d_week_seq = (select d_week_seq
-                         from date_dim
-                         where d_year = 1999 + 1
-                           and d_moy = 12
-                           and d_dom = 3)
-     group by i_brand_id,i_class_id,i_category_id
-     having sum(ss_quantity*ss_list_price) > (select average_sales from avg_sales)) this_year,
-    (select 'store' channel, i_brand_id,i_class_id
-          ,i_category_id, sum(ss_quantity*ss_list_price) sales, count(*) number_sales
-     from store_sales
-        ,item
-        ,date_dim
-     where ss_item_sk in (select ss_item_sk from cross_items)
-       and ss_item_sk = i_item_sk
-       and ss_sold_date_sk = d_date_sk
-       and d_week_seq = (select d_week_seq
-                         from date_dim
-                         where d_year = 1999
-                           and d_moy = 12
-                           and d_dom = 3)
-     group by i_brand_id,i_class_id,i_category_id
-     having sum(ss_quantity*ss_list_price) > (select average_sales from avg_sales)) last_year
-where this_year.i_brand_id= last_year.i_brand_id
-  and this_year.i_class_id = last_year.i_class_id
-  and this_year.i_category_id = last_year.i_category_id
-order by this_year.channel, this_year.i_brand_id, this_year.i_class_id, this_year.i_category_id
-    limit 100;
 
 -- end query 14 in stream 0 using template query14.tpl
 -- start query 15 in stream 0 using template query15.tpl
@@ -1223,57 +1117,8 @@ group by rollup(i_product_name
 order by qoh, i_product_name, i_brand, i_class, i_category
     limit 100;
 
-end query 22 in stream 0 using template query22.tpl
-start query 23 in stream 0 using template query23.tpl
-with frequent_ss_items as
-         (select substr(i_item_desc,1,30) itemdesc,i_item_sk item_sk,d_date solddate,count(*) cnt
-          from store_sales
-             ,date_dim
-             ,item
-          where ss_sold_date_sk = d_date_sk
-            and ss_item_sk = i_item_sk
-            and d_year in (2000,2000+1,2000+2,2000+3)
-          group by substr(i_item_desc,1,30),i_item_sk,d_date
-          having count(*) >4),
-     max_store_sales as
-         (select max(csales) tpcds_cmax
-          from (select c_customer_sk,sum(ss_quantity*ss_sales_price) csales
-                from store_sales
-                   ,customer
-                   ,date_dim
-                where ss_customer_sk = c_customer_sk
-                  and ss_sold_date_sk = d_date_sk
-                  and d_year in (2000,2000+1,2000+2,2000+3)
-                group by c_customer_sk)),
-     best_ss_customer as
-         (select c_customer_sk,sum(ss_quantity*ss_sales_price) ssales
-          from store_sales
-             ,customer
-          where ss_customer_sk = c_customer_sk
-          group by c_customer_sk
-          having sum(ss_quantity*ss_sales_price) > (95/100.0) * (select
-                                                                     *
-                                                                 from
-                                                                     max_store_sales))
-select  sum(sales)
-from (select cs_quantity*cs_list_price sales
-      from catalog_sales
-         ,date_dim
-      where d_year = 2000
-        and d_moy = 7
-        and cs_sold_date_sk = d_date_sk
-        and cs_item_sk in (select item_sk from frequent_ss_items)
-        and cs_bill_customer_sk in (select c_customer_sk from best_ss_customer)
-      union all
-      select ws_quantity*ws_list_price sales
-      from web_sales
-         ,date_dim
-      where d_year = 2000
-        and d_moy = 7
-        and ws_sold_date_sk = d_date_sk
-        and ws_item_sk in (select item_sk from frequent_ss_items)
-        and ws_bill_customer_sk in (select c_customer_sk from best_ss_customer))
-         limit 100;
+-- end query 22 in stream 0 using template query22.tpl
+-- start query 23 in stream 0 using template query23.tpl
 with frequent_ss_items as
          (select substr(i_item_desc,1,30) itemdesc,i_item_sk item_sk,d_date solddate,count(*) cnt
           from store_sales
@@ -1332,58 +1177,6 @@ order by c_last_name,c_first_name,sales
 
 -- end query 23 in stream 0 using template query23.tpl
 -- start query 24 in stream 0 using template query24.tpl
-with ssales as
-         (select c_last_name
-               ,c_first_name
-               ,s_store_name
-               ,ca_state
-               ,s_state
-               ,i_color
-               ,i_current_price
-               ,i_manager_id
-               ,i_units
-               ,i_size
-               ,sum(ss_net_paid) netpaid
-          from store_sales
-             ,store_returns
-             ,store
-             ,item
-             ,customer
-             ,customer_address
-          where ss_ticket_number = sr_ticket_number
-            and ss_item_sk = sr_item_sk
-            and ss_customer_sk = c_customer_sk
-            and ss_item_sk = i_item_sk
-            and ss_store_sk = s_store_sk
-            and c_current_addr_sk = ca_address_sk
-            and c_birth_country <> upper(ca_country)
-            and s_zip = ca_zip
-            and s_market_id=5
-          group by c_last_name
-                 ,c_first_name
-                 ,s_store_name
-                 ,ca_state
-                 ,s_state
-                 ,i_color
-                 ,i_current_price
-                 ,i_manager_id
-                 ,i_units
-                 ,i_size)
-select c_last_name
-     ,c_first_name
-     ,s_store_name
-     ,sum(netpaid) paid
-from ssales
-where i_color = 'aquamarine'
-group by c_last_name
-       ,c_first_name
-       ,s_store_name
-having sum(netpaid) > (select 0.05*avg(netpaid)
-                       from ssales)
-order by c_last_name
-       ,c_first_name
-       ,s_store_name
-;
 with ssales as
          (select c_last_name
                ,c_first_name
@@ -1998,35 +1791,9 @@ where inv1.i_item_sk = inv2.i_item_sk
   and inv1.w_warehouse_sk =  inv2.w_warehouse_sk
   and inv1.d_moy=1
   and inv2.d_moy=1+1
-order by inv1.w_warehouse_sk,inv1.i_item_sk,inv1.d_moy,inv1.mean,inv1.cov
-       ,inv2.d_moy,inv2.mean, inv2.cov
-;
-with inv as
-         (select w_warehouse_name,w_warehouse_sk,i_item_sk,d_moy
-               ,stdev,mean, case mean when 0 then null else stdev/mean end cov
-          from(select w_warehouse_name,w_warehouse_sk,i_item_sk,d_moy
-                    ,stddev_samp(inv_quantity_on_hand) stdev,avg(inv_quantity_on_hand) mean
-               from inventory
-                  ,item
-                  ,warehouse
-                  ,date_dim
-               where inv_item_sk = i_item_sk
-                 and inv_warehouse_sk = w_warehouse_sk
-                 and inv_date_sk = d_date_sk
-                 and d_year =2000
-               group by w_warehouse_name,w_warehouse_sk,i_item_sk,d_moy) foo
-          where case mean when 0 then 0 else stdev/mean end > 1)
-select inv1.w_warehouse_sk,inv1.i_item_sk,inv1.d_moy,inv1.mean, inv1.cov
-     ,inv2.w_warehouse_sk,inv2.i_item_sk,inv2.d_moy,inv2.mean, inv2.cov
-from inv inv1,inv inv2
-where inv1.i_item_sk = inv2.i_item_sk
-  and inv1.w_warehouse_sk =  inv2.w_warehouse_sk
-  and inv1.d_moy=1
-  and inv2.d_moy=1+1
   and inv1.cov > 1.5
 order by inv1.w_warehouse_sk,inv1.i_item_sk,inv1.d_moy,inv1.mean,inv1.cov
-       ,inv2.d_moy,inv2.mean, inv2.cov
-;
+       ,inv2.d_moy,inv2.mean, inv2.cov;
 
 -- end query 39 in stream 0 using template query39.tpl
 -- start query 40 in stream 0 using template query40.tpl
